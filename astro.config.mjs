1c3099b150c8c83ee8fe0eac4abc313be97ba368
glokta1 <rafeyahmad@protonmail.com>
2023-07-29 20:37:18 +0530

    Add deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
+  site: "https://glokta1.github.io",
+  base: "/ssamusat",
 });
