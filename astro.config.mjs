35998168549a504e4f3932f331f58f034ee2f3c8
glokta1 <rafeyahmad@protonmail.com>
2023-07-29 20:39:16 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
   site: "https://glokta1.github.io",
   base: "/ssamusat",
 });
